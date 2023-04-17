8c20bd37a8763227c39b2173ef3cdd2368a1d663
Yung Cho <yung5060@kakao.com>
2023-04-17 22:33:52 +0900

    20230417

--- spring-batch/src/main/java/com/example/demo/JobInstanceConfiguration.java
+++ spring-batch/src/main/java/com/example/demo/JobInstanceConfiguration.java
@@ -13,46 +13,37 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class JobConfiguration {
+public class JobInstanceConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
-	
+
 	@Bean
 	public Job job() {
-		return jobBuilderFactory.get("job")
-				.start(step1())
-				.next(step2())
-				.build();
+		return jobBuilderFactory.get("job").start(step1()).next(step2()).build();
 	}
-	
+
 	@Bean
 	public Step step1() {
-		return stepBuilderFactory.get("step1")
-				.tasklet(new Tasklet() {
-					
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("step1 has been executed!");
-						return RepeatStatus.FINISHED;
-					}
-				})
-				.build();
+		return stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
 	}
-	
+
 	@Bean
 	public Step step2() {
-		return stepBuilderFactory.get("step2")
-				.tasklet(new Tasklet() {
-					
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("step2 has been executed!");
-						return RepeatStatus.FINISHED;
-					}
-				})
-				.build();
+		return stepBuilderFactory.get("step2").tasklet(new Tasklet() {
+
+				@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
 	}
 }
