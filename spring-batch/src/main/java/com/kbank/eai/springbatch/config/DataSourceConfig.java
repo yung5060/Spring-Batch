ff18d194e525562a72f3ef39be57a42508a444cc
Yung Cho <yung5060@kakao.com>
2023-04-18 23:42:12 +0900

    .

---
+++ spring-batch/src/main/java/com/kbank/eai/springbatch/config/DataSourceConfig.java
@@ -0,0 +1,62 @@
+package com.kbank.eai.springbatch.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.kbank.eai.springbatch.dto.DataSourceInfo;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataSourceConfig {
+
+	private final YamlConfig yamlConfig;
+	
+	@Primary
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		DataSource ds = null;
+		return findDs("LOCAL", ds);
+	}
+
+	@Bean(name = "logDataSource")
+	public DataSource logDataSource() throws SQLException {
+		DataSource ds = null;
+		return findDs("LOG", ds);
+	}
+
+	@Bean(name = "srcDataSource")
+	public DataSource srcDataSource(@Value("${sBiz}") String sBiz) throws SQLException {
+		DataSource ds = null;
+		return findDs(sBiz, ds);
+	}
+
+	@Bean(name = "dstDataSource")
+	public DataSource dstDataSource(@Value("${dBiz}") String dBiz) throws SQLException {
+		DataSource ds = null;
+		return findDs(dBiz, ds);
+	}
+
+	private DataSource findDs(String biz, DataSource ds) throws SQLException {
+		for (DataSourceInfo info : yamlConfig.getList()) {
+			if(info.getSystem().equals(biz)) {
+				ds = DataSourceBuilder.create()
+						.driverClassName(info.getDriverClassName())
+						.url(info.getUrl())
+						.username(info.getUsername())
+						.password(info.getPassword())
+						.build();
+				return ds;
+			}
+		}
+		return ds;
+	}
+}
---
+++ spring-batch/src/main/java/com/kbank/eai/springbatch/dto/DataSourceInfo.java
@@ -0,0 +1,19 @@
+package com.kbank.eai.springbatch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataSourceInfo {
+
+	private String system;
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+}
