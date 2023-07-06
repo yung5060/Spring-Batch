6ebd1638c54552ae89abb9a1d82e04944e9263a9
Yung Cho <yung5060@kakao.com>
2023-07-06 23:21:45 +0900

    20230706

--- kbank-spring-batch/src/main/java/com/kbank/eai/config/ChunkConfig.java
+++ kbank-spring-batch/src/main/java/com/kbank/eai/config/ChunkConfig.java
@@ -5,11 +5,8 @@ import java.util.List;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
@@ -39,23 +36,20 @@ public class ChunkConfig {
 	@Bean
 	public Step step1() {
 		return stepBuilderFactory.get("step1")
-				.<String, String>chunk(5)
-				.reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+				.<String, String>chunk(2)
+				.reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5", "item6")))
 				.processor(new ItemProcessor<String, String>() {
 					@Override
 					@Nullable
 					public String process(@NonNull String item) throws Exception {
-						Thread.sleep(300);
-						System.out.println("item = " + item);
-						return "my" + item;
+						return "my_" + item;
 					}
 					
 				})
 				.writer(new ItemWriter<String>() {
 					@Override
 					public void write(List<? extends String> items) throws Exception {
-						Thread.sleep(300);
-						System.out.println("items = " + items);
+						items.forEach(item -> System.out.println(item));
 					}
 				})
 				.build();
