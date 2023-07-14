f4a903c7f68fbb69412f7f3fce56c15c8b334a4e
Yung Cho <yung5060@kakao.com>
2023-07-14 22:38:13 +0900

    20230714

--- kbank-spring-batch/src/main/java/com/kbank/eai/util/CustomItemProcessor.java
+++ kbank-spring-batch/src/main/java/com/kbank/eai/util/CustomItemProcessor.java
@@ -3,11 +3,12 @@ package com.kbank.eai.util;
 
 import com.kbank.eai.entity.Customer;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.lang.NonNull;
 
 public class CustomItemProcessor implements ItemProcessor<Customer, Customer> {
 
     @Override
-    public Customer process(Customer item) throws Exception {
+    public Customer process(@NonNull Customer item) throws Exception {
 
         item.setName(item.getName().toUpperCase());
 
