b64f581596c178d5b441fb8b6998842484f058a5
Yung Cho <yung5060@kakao.com>
2023-09-03 23:01:34 +0900

    20230903

--- inflearn-batch/src/main/java/com/kbank/eai/InflearnBatchApplication.java
+++ inflearn-batch/src/main/java/com/kbank/eai/InflearnBatchApplication.java
@@ -19,8 +19,8 @@ public class InflearnBatchApplication {
 
 	public static void main(String[] args) throws Exception {
 		
-//		loadJar(args[0]); 	//Windows
-		loadJar(args[1]);	//Mac
+		loadJar(args[0]); 	//Windows
+//		loadJar(args[1]);	//Mac
 		
 		SpringApplication.run(InflearnBatchApplication.class, args);
 	}
